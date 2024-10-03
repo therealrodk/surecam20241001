1bb20c3de013fdf1bbd12b7eab3fb4549f60c85d
Rod Klingler <rod@theklinglerfamily.com>
2024-10-03 13:21:26 -0600

    Update index; add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Users
+User.create!(email: "bob@test.com", password: "password", password_confirmation: "password") unless User.find_by(email: "bob@test.com")
+User.create!(email: "fred@test.com", password: "password", password_confirmation: "password") unless User.find_by(email: "fred@test.com")
+
+# Blog Posts
+Post.create!(user_id: User.first.id, title: "Public post by #{User.first.email}", body: "<p>Stuff goes here</p>")
+Post.create!(user_id: User.last.id, title: "Public post by #{User.last.email}", body: "<p>Stuff goes here</p>")
+Post.create!(user_id: User.first.id, title: "Private post by #{User.first.email}", body: "<p>Stuff goes here</p>", active: false)
+Post.create!(user_id: User.last.id, title: "Private post by #{User.last.email}", body: "<p>Stuff goes here</p>", active: false)
