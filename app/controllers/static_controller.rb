cc9d49a8e97ccd09b44969aa33aaa94d403beb8f
Rod Klingler <rod@theklinglerfamily.com>
2024-10-03 13:25:42 -0600

    Conditionally display messages on index.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,7 @@ class StaticController < ApplicationController
   def index
     if user_signed_in?
       @posts = params[:view_all] == "true" ? Post.active.newest_first : @posts = current_user.posts.newest_first
+      @user_created = true
     else
       @posts = Post.active.newest_first
     end
